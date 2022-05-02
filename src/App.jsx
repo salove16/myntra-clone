a0a19e214bda1c4ee3721f42fb8580d28c751330
deekshant57 <80623479+deekshant57@users.noreply.github.com>
2022-05-02 23:52:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to home page</h1>
+      {/* {Links} */}
+      {/* {Routes} */}
+    </div>
+  );
+}
+
+export default App;
