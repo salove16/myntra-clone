180bcb255423d72852e6f105746eabae7aa8bf34
deekshant57 <80623479+deekshant57@users.noreply.github.com>
2022-05-03 00:10:53 +0530

    Added desc

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ function App() {
       <h1>Welcome to home page</h1>
       {/* {Links} */}
       {/* {Routes} */}
+
+      {/* {Few things which needs to be discussed before starting off with the project
+        1) CSS library
+        2) Redux / Context API
+        3) Hardcode the data or API
+        4) Payment gateway
+      } */}
     </div>
   );
 }
