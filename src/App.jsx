1505b4c75ef485ccd8d63adde0f79ca1560e6211
salove16 <shashank.shalabh909@gmail.com>
2022-05-05 11:19:19 +0530

    Navbar done and routes chanaged from main to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import {Navbar} from "./Components/Navbar/Navbar"
+import MenSection from "./Components/MenSection";
+
+import {Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to home page</h1>
-      {/* {Links} */}
+     <Navbar />
+      <Routes>
+        <Route path="/" ></Route>
+        <Route path="/mens" element={<MenSection></MenSection>}></Route>
+      </Routes>
+      {/* {Few things which needs to be discussed before starting off with the project
+        0) How to contribute - 
+        1) CSS library - Ant design, Material UI, Bootstrap.  - Material UI
+        2) Redux / Context API -  Context API.
+        3) Hardcode the data or API - Zara API.
+
+
+        4) Payment gateway - 
+      } */}
     </div>
   );
 }
