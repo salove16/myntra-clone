33260587d178d1c0b40931c2602ea21b6083b567
Neelam <neelam0101singh@gmail.com>
2022-05-07 08:14:15 +0530

    home css updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 
 import {Navbar} from "./Components/Navbar/Navbar"
 import MenSection from "./Components/MenSection";
-
+import {Home} from "./Components/Home/Home"
+import {Footer} from "./Components/Footer/Footer"
 import {Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
      <Navbar />
       <Routes>
-        <Route path="/" ></Route>
+        <Route path="/" element={<Home></Home>} ></Route>
         <Route path="/mens" element={<MenSection></MenSection>}></Route>
       </Routes>
-      {/* {Few things which needs to be discussed before starting off with the project
-        0) How to contribute - 
-        1) CSS library - Ant design, Material UI, Bootstrap.  - Material UI
-        2) Redux / Context API -  Context API.
-        3) Hardcode the data or API - Zara API.
 
 
-        4) Payment gateway - 
-      } */}
-
+      
+       <Footer></Footer>
     </div>
   );
 }
